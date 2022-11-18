fc83aa939329a1ae07a7b1f1364facab291e975e
Ugur Gurler <108265528+CtTaeUG@users.noreply.github.com>
2022-11-18 09:38:59 +0300

    Complete the test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,34 @@
 package tudelft.gettingstarted;
 
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
     }
+    @Test
+    public void addFiveToTwenty() {
+        int result = new GettingStarted().addFive(20);
+        Assertions.assertEquals(25,result);
+    }
+
+    @Test
+    public void addFiveToZero(){
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinusTwenty() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
+    }
+
+
+
+
+
+
+
 }
